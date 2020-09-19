7dfadf21695e01776540c216de1080a6e6bf828d
Gurkiran Singh <gurkiransinghk@gmail.com>
2020-09-19 03:16:58 -0400

    copy functionality is also added

--- src/App.js
+++ src/App.js
@@ -6,9 +6,8 @@ function App() {
   const [slideValue, setSlideValue] = useState(20);
   const [divValue, setDivValue] = useState(1);
   const [password, setPassword] = useState("");
-  const [decreaseValue, setDecreaseValue] = useState(31);
-
-  console.log( "Decrease Value",decreaseValue)
+  const [decreaseValue, setDecreaseValue] = useState(28);
+  const [label, setLabel] = useState("Strong");
 
   const divValueRef = useRef();
   const slideValueRef = useRef();
@@ -17,6 +16,8 @@ function App() {
   const uppercaseRef = useRef();
   const lowercaseRef = useRef();
   const passwordRef = useRef();
+  const passwordContainerRef = useRef();
+  const labelIconRef = useRef();
 
 
   useEffect(() => {
@@ -43,8 +44,18 @@ function App() {
       passwordRef.current.style.fontSize = `${decreaseValue}px`;
       setDecreaseValue(decreaseValue - 1);
     }
-    else if(slideValue < 28){
-      passwordRef.current.style.fontSize = "32px";
+    
+    if(slideValue < 11 && slideValue > 6){
+      passwordContainerRef.current.style.backgroundColor = "#f9622f";
+      setLabel("Weak");
+      labelIconRef.current.style.backgroundPosition = "20px 0px";
+    }else if(slideValue < 6 || slideValue === 6){
+      passwordContainerRef.current.style.backgroundColor = "#c81a00";
+      setLabel("Bad");
+    }else if(slideValue < 47){
+      passwordContainerRef.current.style.backgroundColor = " #339933";
+      setLabel("Strong");
+      labelIconRef.current.style.backgroundPosition = "initial";
     }
 
     const length = slideValue;
@@ -71,7 +82,7 @@ function App() {
   const slideChanger = (e) => {
     let targetValue = parseInt(e.target.value);
 
-    if((slideValue > targetValue ) && (decreaseValue !== 32)){
+    if((slideValue > targetValue ) && (decreaseValue !== 31)){
       passwordRef.current.style.fontSize = `${decreaseValue}px`
       setDecreaseValue(decreaseValue + 1)
     }
@@ -79,16 +90,27 @@ function App() {
     setSlideValue(targetValue);
   }
 
+  const copyPassword = () => {
+    let selection = window.getSelection();
+    selection.removeAllRanges();
+
+    let range = new Range();
+    range.selectNodeContents(passwordRef.current);
+    selection.addRange(range);
+
+    document.execCommand("copy");
+  }
+
   const uppercase = () => {
-    return String.fromCharCode(Math.floor(Math.random()*26)+65);
+    return String.fromCharCode(Math.floor(Math.random()*26) + 65);
   }
 
   const lowercase = () => {
-    return String.fromCharCode(Math.floor(Math.random()*26)+97);
+    return String.fromCharCode(Math.floor(Math.random()*26) + 97);
   }
 
   const numbers = () => {
-    return String.fromCharCode(Math.floor(Math.random()*10)+48);
+    return String.fromCharCode(Math.floor(Math.random()*10) + 48);
   }
 
   const symbols = () => {
@@ -132,17 +154,17 @@ function App() {
           <h1>Create a strong password with Password Generator</h1>
         </header>
 
-        <main className="password-gen-container">
+        <main ref={passwordContainerRef} className="password-gen-container">
           <div>
             <div ref={passwordRef} className="password"> {password} </div>
             <div>
               <div onClick={checkedFunc} className="reset-pass"></div>
-              <div className="copy-pass">Copy&nbsp;Password</div>
+              <div onClick={copyPassword} className="copy-pass">Copy&nbsp;Password</div>
             </div>
           </div>
           <div className="labels-container">
-            <span className="pass-label-icon icon"></span>
-            <span className="pass-label">Strong Password</span>
+            <span ref={labelIconRef} className="pass-label-icon icon"></span>
+            <span className="pass-label">{label} Password</span>
           </div>
         </main>
 
@@ -170,7 +192,7 @@ function App() {
           </label>
 
           <div className="slide-container">
-            <p>Password Length (4-64)</p>
+            <p>Password Length (4-47)</p>
             <div ref={divValueRef} className="range-value">{slideValue}</div>
             <input ref={slideValueRef} onChange={slideChanger} className="slide-range" step="1" type="range" min="4" max="47" value={slideValue}  />
           </div>
